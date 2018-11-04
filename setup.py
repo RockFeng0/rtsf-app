6bf75a47440cbd6a2c155ed9d204180cfa70af7f
RockFeng0 <lkf20031988@163.com>
2018-11-04 09:48:18 +0800

    update version to v1.2, support data-driven test

--- setup.py
+++ setup.py
@@ -21,8 +21,56 @@ Provide a function for the automation test
 
 
 from appuidriver import __about__
-from setuptools import setup, find_packages
+import os, io, sys
+from shutil import rmtree
+from setuptools import setup, find_packages, Command
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
+
+class UploadCommand(Command):
+    """ Build and publish this package.
+        Support setup.py upload. Copied from requests_html.
+    """
+
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in green color."""
+        print("\033[0;32m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        """ override
+        """
+        pass
+
+    def finalize_options(self):
+        """ override
+        """
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Publishing git tags…')
+        os.system('git tag v{0}'.format(__about__.__version__))
+        os.system('git push --tags')
+
+        sys.exit()
+        
 install_requires = [
     "Appium-Python-Client",
     "requests",
@@ -50,11 +98,15 @@ setup(
 #         dependency_links=dependency_links,
         extras_require={},
         entry_points={
-        'console_scripts': [
-            'aldriver=appuidriver.cli:local_main_run', # one appium server <--> only one android device  
-            'ardriver=appuidriver.cli:remote_main_run', # appium nodes <--> android devices          
-            'appserver=appuidriver.cli:appium_main_run', # appium server, appium node for grid mode
-            'ainfo=appuidriver.cli:tools_main_run', # devices info or apk info
-        ]
-    },
+            'console_scripts': [
+                'aldriver=appuidriver.cli:local_main_run', # one appium server <--> only one android device  
+                'ardriver=appuidriver.cli:remote_main_run', # appium nodes <--> android devices          
+                'appserver=appuidriver.cli:appium_main_run', # appium server, appium node for grid mode
+                'ainfo=appuidriver.cli:tools_main_run', # devices info or apk info
+            ]
+        },
+        # $ setup.py upload support.
+        cmdclass={
+            'upload': UploadCommand
+        }
     )
