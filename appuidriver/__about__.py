9b61c3ec3ef13d9f38c43942fd48fa4b29bf7ff7
rockfeng0 <lkf20031988@163.com>
2023-08-15 17:29:48 +0800

    feat: version 1.2.4

--- appuidriver/__about__.py
+++ appuidriver/__about__.py
@@ -3,7 +3,7 @@
 
 __title__ = "rtsf-app"
 __short_desc__ = "only for android ui test, base on rtsf"
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 __autor__ = u"罗科峰"
 __author_email__ = "lkf20031988@163.com"
 __license__ = 'MIT'
